047cb0de13d420207c05dd50cc6d8f87c3056d0c
pingan2090-lgtm <pingan2090@gmail.com>
2026-09-19 15:32:17 -0700

    Update problems.js

--- lesson1/homework/problems.js
+++ lesson1/homework/problems.js
@@ -1,25 +1,30 @@
 // Problem 1
 // Create a variable for your lucky number and print it.
 
-
+let Word = 4;
+console.log (Word);
 
 // Problem 2
 // Create a variable for the number of hours you sleep and print it.
 
+let time = 8;
+console.log (time)
 
 
 // Problem 3
 // Create a variable for your favorite fruit and print it in a full sentence.
 
-
+let cool = "mango";
+console.log (cool);
 
 // Problem 4
 // Create two variables: one for your city and one for your country.
 // Print them on 2 separate lines.
-
-
-
+let haha = "seattle";
+console.log (haha);
+let weird = "USA"
+console.log (weird)
 // Problem 5
 // Create 3 variables: your pet's name, its age, and its type.
 // Print them on the same line in a full sentence.
-
+console.log ('I like' ,cool, 'I live in' ,haha,'which is in a country called' ,weird)
